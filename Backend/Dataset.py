66792b05ee7dbfaef76b95afd6e00bb009f954c1
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-10-29 15:16:54 +0100

    added get_image_path

--- Backend/Dataset.py
+++ Backend/Dataset.py
@@ -1,5 +1,5 @@
 import os
-import glob as gb
+import glob
 from torchvision import transforms
 from PIL import Image
 from torch.utils.data import DataLoader
@@ -7,4 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 DATASET_DIR = "C:\Users\lizak\Data_Science\Semester_5\Advanced_IS\Project_Data\img_align_celeba\img_align_celeba"
+
+def get_image_path(dir):
+    return sorted(glob.glob(os.path.join(dir, "*.jpg")))
+
 
