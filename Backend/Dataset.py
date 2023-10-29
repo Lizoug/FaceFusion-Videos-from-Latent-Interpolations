8f227aa94e80eac76d63993175cf293105230b93
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-10-29 15:26:29 +0100

    added the function transform

--- Backend/Dataset.py
+++ Backend/Dataset.py
@@ -12,3 +12,12 @@ def get_image_path(dir):
     return sorted(glob.glob(os.path.join(dir, "*.jpg")))
 
 
+def transform(size):
+    return transforms.Compose([
+        transforms.Resize(size=(size, size)),
+        # [0, 1]
+        transforms.ToTensor(),
+        transforms.CenterCrop(size),
+        # [-1, 1]
+        transforms.Normalize([0.5], [0.5])
+    ])
