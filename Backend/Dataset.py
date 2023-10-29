8036b79ba13cd574931b7840953926d5bdc88bab
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-10-29 15:05:42 +0100

    added inouts and data_dir

---
+++ Backend/Dataset.py
@@ -0,0 +1,10 @@
+import os
+import glob as gb
+from torchvision import transforms
+from PIL import Image
+from torch.utils.data import DataLoader
+import numpy as np
+import matplotlib.pyplot as plt
+
+DATASET_DIR = "C:\Users\lizak\Data_Science\Semester_5\Advanced_IS\Project_Data\img_align_celeba\img_align_celeba"
+
