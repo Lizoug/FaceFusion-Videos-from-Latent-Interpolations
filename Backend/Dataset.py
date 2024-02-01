a1b72fafaf617dabe2ca06ba8d1cc5545e813194
Liza Kaladjian <lizakalaijiann@gmail.com>
2024-02-01 01:00:53 +0100

    flake8 in dataset

--- Backend/Dataset.py
+++ Backend/Dataset.py
@@ -11,11 +11,12 @@ import matplotlib.pyplot as plt
 class CelebADataset(Dataset):
     def __init__(self, dir, transform=None):
         # Get all image file paths from the given directory
-        # find all files in the specified directory that match the given pattern. 
-        # In this case, it's looking for all .jpg files in the specified directory. 
-        # The function returns a list of file paths that match the pattern.
+        # find all files in the specified directory that match the given
+        # pattern. In this case, it's looking for all .jpg files in the
+        # specified directory. The function returns a list of file paths that
+        # match the pattern.
         self.image_paths = sorted(glob.glob(os.path.join(dir, "*.jpg")))
-        self.transform = transform # Set the image transformation method
+        self.transform = transform  # Set the image transformation method
 
     def __len__(self):
         # Return the total number of images
@@ -23,40 +24,40 @@ class CelebADataset(Dataset):
 
     def __getitem__(self, idx):
         # Get an image by index and apply the transformation
-        img_path = self.image_paths[idx] # image path
-        image = Image.open(img_path) # open image
+        img_path = self.image_paths[idx]  # image path
+        image = Image.open(img_path)  # open image
         if self.transform:
-            image = self.transform(image) # apply transformation
+            image = self.transform(image)  # apply transformation
         return image
-    
+
 
 # Function to apply transformations to the images
 def transform(size):
     return transforms.Compose([
-        transforms.Resize(size=(size, size)), # resizing the image
+        transforms.Resize(size=(size, size)),  # resizing the image
         # [0, 1]
-        transforms.ToTensor(), # Converting image to PyTorch tensor
-        transforms.CenterCrop(size), # Croping the image
+        transforms.ToTensor(),  # Converting image to PyTorch tensor
+        transforms.CenterCrop(size),  # Croping the image
         # [-1, 1]
-        transforms.Normalize([0.5], [0.5]) # Normalize the image
+        transforms.Normalize([0.5], [0.5])  # Normalize the image
     ])
 
 
 if __name__ == "__main__":
-    IMG_SIZE = 128 # Set the image size
+    IMG_SIZE = 128  # Set the image size
     # Directory where the dataset is located
     DATASET_DIR = r"C:\Users\lizak\Data_Science\Semester_5\Advanced_IS\Project_Data\img_align_celeba\img_align_celeba"
-    
+
     # Get the transformation pipelin
-    transform_pipeline = transform(IMG_SIZE) 
-    
+    transform_pipeline = transform(IMG_SIZE)
+
     # Create an instance of the dataset
     dataset = CelebADataset(DATASET_DIR, transform=transform_pipeline)
-    BATCH_SIZE = 32 # batch size for data loading
-    
+    BATCH_SIZE = 32  # batch size for data loading
+
     # DataLoader to load the dataset in batches
-    data_loader = DataLoader(dataset=dataset, 
-                             batch_size=BATCH_SIZE, 
+    data_loader = DataLoader(dataset=dataset,
+                             batch_size=BATCH_SIZE,
                              shuffle=True)
 
     print(f"Total batches in dataloader: {len(data_loader)} with batch size of {BATCH_SIZE}")
@@ -64,12 +65,10 @@ if __name__ == "__main__":
     # Display sample images
     sample_imgs = next(iter(data_loader))
 
-    figure, axes = plt.subplots(3, 5, figsize=(14, 9)) #subplot
+    figure, axes = plt.subplots(3, 5, figsize=(14, 9))  # subplot
     for idx, ax in enumerate(axes.flat):
-        image_array = sample_imgs[idx].numpy() # converting tensor to numpy array
-        image_array = np.transpose(image_array, (1, 2, 0)) # Transpose array dimensions
-        ax.imshow(image_array) # Show image
-        ax.axis('off') # hide axis
-    plt.show() # display the plot
-
-
+        image_array = sample_imgs[idx].numpy()  # converting tensor to np array
+        image_array = np.transpose(image_array, (1, 2, 0))  # Transpose array dimensions
+        ax.imshow(image_array)  # Show image
+        ax.axis('off')  # hide axis
+    plt.show()  # display the plot
